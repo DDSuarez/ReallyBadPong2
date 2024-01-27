c3806c0fb094fcdfc5fdd0f4a1dd4f7e8f2da6c0
Danny Suarez <metasaval@gmail.com>
2024-01-26 19:17:56 -0800

    now make it funny. made ball it's own scene. added button that changes background color and button that spawns a new ball.

--- field.gd
+++ field.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
-var score := [0, 0] # 0:Player, 1: CPU
+@onready var screen_size = get_viewport_rect().size
+
 const PADDLE_SPEED: int = 500
+
+var score := [0, 0] # 0:Player, 1: CPU
+
+var ballScene = preload("res://ball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,8 +15,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("spawn"):
-		$Ball.new_ball()
+	if Input.is_action_just_pressed("spawn"):
+		var newBall = ballScene.instantiate()
+		newBall.position.x = screen_size.x / 2
+		newBall.position.y = randi_range(200, screen_size.y - 200)
+		newBall.speed = newBall.START_SPEED
+		newBall.dir = newBall.random_direction()
+		add_child(newBall)
+	if Input.is_action_just_pressed("rave"):
+		$Background.set_color(Color(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1), randf_range(0, 1)))
 
 
 func _on_ball_timer_timeout():
